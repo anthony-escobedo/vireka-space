c7aa2e8d7bdb792bc29c7c0a94d2d392e0f622dc
anthony-escobedo <anthony.esc49@gmail.com>
2026-04-13 07:45:52 -0400

    Update page.tsx

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -53,8 +53,8 @@ export default function HomePage() {
           <nav
   style={{
     display: "flex",
-    gap: "14px",
-    fontSize: "15px",
+    gap: "10px",
+    fontSize: "13px",
     flexWrap: "wrap",
     justifyContent: "flex-end",
     maxWidth: "100%",

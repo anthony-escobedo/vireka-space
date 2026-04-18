5ad8b3c7c3f2cd572f733117680b5eb7045ef9c1
anthony-escobedo <anthony.esc49@gmail.com>
2026-04-17 22:03:21 -0400

    Update page.tsx

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -200,7 +200,7 @@ export default function HomePage() {
                   style={{
                     position: "absolute",
                     top: "52px",
-                    right: -100,
+                    right: -110,
                     minWidth: "220px",
                     padding: "8px",
                     borderRadius: "16px",

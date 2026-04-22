f0ab1e659935077a3bd3966765585654806c7d0b
anthony-escobedo <anthony.esc49@gmail.com>
2026-04-22 09:37:06 -0400

    Create middleware.ts

---
+++ frontend/middleware.ts
@@ -0,0 +1,29 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+export function middleware(request: NextRequest) {
+  const basicAuth = request.headers.get('authorization')
+
+  const username = 'admin'
+  const password = 'vireka'
+
+  if (basicAuth) {
+    const authValue = basicAuth.split(' ')[1]
+    const [user, pass] = atob(authValue).split(':')
+
+    if (user === username && pass === password) {
+      return NextResponse.next()
+    }
+  }
+
+  return new Response('Authentication required', {
+    status: 401,
+    headers: {
+      'WWW-Authenticate': 'Basic realm="Secure Area"',
+    },
+  })
+}
+
+export const config = {
+  matcher: '/:path*',
+}
